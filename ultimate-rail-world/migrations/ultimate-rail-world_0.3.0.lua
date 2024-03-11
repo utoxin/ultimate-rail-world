f3edcad5f9fa2a5884eed4225ff74191bdd18b52
Matthew Walker <utoxin@gmail.com>
2024-03-10 20:19:49 -0600

    Adding migrations
    Re-adding the 0.2.0 migration, and adding a new 0.3.0 migration.

---
+++ ultimate-rail-world/migrations/ultimate-rail-world_0.3.0.lua
@@ -0,0 +1,5 @@
+for i, force in pairs(game.forces) do
+    force.technologies["logistics"].hidden = true
+    force.technologies["logistics-2"].hidden = true
+    force.technologies["logistics-3"].hidden = true
+end
